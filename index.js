433840636f8a61820ef532d7339b9e613ec9ebba
Moin-6k <moin6k@gmail.com>
2022-11-25 00:58:00 +0530

    navbar | home | about added

---
+++ index.js
@@ -0,0 +1,9 @@
+let linkedin = document.getElementById("linkedin");
+linkedin.onclick = function(){
+    window.location.href = "https://www.linkedin.com/in/moinkhan07/";
+}
+
+let github = document.getElementById("github");
+github.onclick = function(){
+    window.location.href = "https://github.com/moinkhan07";
+}

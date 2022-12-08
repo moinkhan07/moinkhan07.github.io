4ae8f8e1c5ddc681159b3ffa69dff511c800206a
Moin-6k <moin6k@gmail.com>
2022-12-08 15:48:26 +0530

    Update

--- index.js
+++ index.js
@@ -7,3 +7,12 @@
 // github.onclick = function(){
 //     window.location.href = "https://github.com/moinkhan07";
 // }
+
+let sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-250px";
+}
